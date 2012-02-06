5873b69d5fcb1edc8381ff53707f90f01e771b0a
Tassadar <vbocek@gmail.com>
2012-02-06 23:05:10 +0100

    Add czech translation

---
+++ cz/packer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm index.html
+
+files=( head.html menu.html downloads.html desc.html build.html license.html screenshots.html menu.html bottom.html )
+for file in ${files[@]}
+do
+    echo "<!-- $file -->" >> index.html
+    cat $file >> index.html
+    echo "" >> index.html
+done
