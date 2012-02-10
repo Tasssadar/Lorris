0e064085cdcb162f24d4535d83a359ea2e6250a3
Tassadar <vbocek@gmail.com>
2012-02-10 13:33:50 +0100

    Add bug report

--- packer.sh
+++ packer.sh
@@ -2,7 +2,7 @@
 
 rm index.html
 
-files=( head.html menu.html downloads.html desc.html build.html license.html screenshots.html bottom.html )
+files=( head.html menu.html downloads.html desc.html build.html license.html bug.html screenshots.html bottom.html )
 for file in ${files[@]}
 do
     echo "<!-- $file -->" >> index.html
