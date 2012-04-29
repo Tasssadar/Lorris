187dc4dceb67e11c377704117a6fa9174de797a9
Tassadar <vbocek@gmail.com>
2012-04-29 22:12:45 +0200

    [331] Split main() into multiple methods

--- src/main.cpp
+++ src/main.cpp
@@ -34,54 +34,46 @@
 #include "connection/connectionmgr2.h"
 #include "WorkTab/WorkTabMgr.h"
 
-int main(int argc, char *argv[])
+static bool checkArgs(int argc, char** argv, QStringList& openFiles)
 {
-    // Sort tab infos after they were added by static variables
-    sWorkTabMgr.SortTabInfos();
+    if(argc < 2)
+        return true;
 
-    QStringList openFiles;
-    if(argc != 1)
+    for(int i = 1; i < argc; ++i)
     {
-        for(int i = 1; i < argc; ++i)
+        if(strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0)
         {
-            if(strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0)
-            {
-                printf("Lorris release %s, git revision %u\n", VERSION, REVISION);
-                return 0;
-            }
-            else if(strcmp(argv[i], "--help") == 0)
-            {
-                printf("Usage: %s [ARGUMENTS...]  Run Lorris\n\n"
-                    "Lorris, GUI tool for robotics - https://github.com/Tasssadar/Lorris\n\n"
-                    "Command line argumens:\n"
-                    "       %s --help          Display this help and exit\n"
-                    "       %s --version       Display version info and exit\n"
-                    "       %s -v\n",
-                    argv[0], argv[0], argv[0], argv[0]);
-                return 0;
-            }
-            else
-            {
-                QString filename(argv[i]);
-                QStringList parts = filename.split(".", QString::SkipEmptyParts);
+            printf("Lorris release %s, git revision %u\n", VERSION, REVISION);
+            return false;
+        }
+        else if(strcmp(argv[i], "--help") == 0)
+        {
+            printf("Usage: %s [ARGUMENTS...]  Run Lorris\n\n"
+                "Lorris, GUI tool for robotics - https://github.com/Tasssadar/Lorris\n\n"
+                 "Command line argumens:\n"
+                "       %s --help          Display this help and exit\n"
+                "       %s --version       Display version info and exit\n"
+                "       %s -v\n",
+                argv[0], argv[0], argv[0], argv[0]);
+            return false;
+        }
+        else
+        {
+            QString filename(argv[i]);
+            QStringList parts = filename.split(".", QString::SkipEmptyParts);
 
-                if(parts.empty())
-                    continue;
+            if(parts.empty())
+               continue;
 
-                if(sWorkTabMgr.isFileHandled(parts.back()))
-                    openFiles << filename;
-            }
+            if(sWorkTabMgr.isFileHandled(parts.back()))
+                openFiles << filename;
         }
     }
+    return true;
+}
 
-    QApplication a(argc, argv);
-    psConMgr2 = new ConnectionManager2(&a);
-
-    //TODO: found an organization
-    //QCoreApplication::setOrganizationName("-");
-    QCoreApplication::setOrganizationDomain("github.com/Tasssadar");
-    QCoreApplication::setApplicationName("Lorris");
-
+static void installTranslator(QApplication& a)
+{
     QLocale newLang = QLocale(langs[sConfig.get(CFG_QUINT32_LANGUAGE)]);
 
     QTranslator qtTranslator;
@@ -95,6 +87,25 @@ int main(int argc, char *argv[])
 
     if(loaded)
         a.installTranslator(&translator);
+}
+
+int main(int argc, char *argv[])
+{
+    //TODO: found an organization
+    //QCoreApplication::setOrganizationName("-");
+    QCoreApplication::setOrganizationDomain("github.com/Tasssadar");
+    QCoreApplication::setApplicationName("Lorris");
+
+    // Sort tab infos after they were added by static variables
+    sWorkTabMgr.SortTabInfos();
+
+    QStringList openFiles;
+    if(!checkArgs(argc, argv, openFiles))
+        return 0;
+
+    QApplication a(argc, argv);
+    psConMgr2 = new ConnectionManager2(&a);
+    installTranslator(a);
 
     MainWindow w;
     w.show(openFiles);
--- src/revision.h
+++ src/revision.h
@@ -24,5 +24,5 @@
 #ifndef REVISION_H
 #define REVISION_H
  #define VERSION "0.4.0-dev"
- #define REVISION 330
+ #define REVISION 331
 #endif // REVISION_H
