009c3c5c1ef6742f754c2132c55f980bb65a00c2
Tassadar <vbocek@gmail.com>
2012-05-10 23:53:48 +0200

    Remove outdated comments from singleton.h
    * It is completely different now

--- src/singleton.h
+++ src/singleton.h
@@ -5,19 +5,18 @@
 **    See README and COPYING
 ***********************************************/
 
-// (c) AbyssX Group
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
-//! Scott Bilas' Singleton implementation, with proper casting mod.
-template <class T> class Singleton
+template <class T>
+class Singleton
 {
-    public:
-        static T &GetSingleton(void)
-        {
-            static T singleton;
-            return singleton;
-        }
+public:
+    static T &GetSingleton()
+    {
+        static T singleton;
+        return singleton;
+    }
 };
 
 #endif
