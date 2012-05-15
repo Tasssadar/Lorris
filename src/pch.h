136eabe7747dd0c3278cd4775f8154765d4aecd5
Tassadar <vbocek@gmail.com>
2012-05-15 14:54:33 +0200

    Remove useless "#if defined __cplusplus"

--- src/pch.h
+++ src/pch.h
@@ -5,20 +5,18 @@
 **    See README and COPYING
 ***********************************************/
 
-#if defined __cplusplus
- #include <vector>
- #include <set>
- #include <QLabel>
- #include <QPushButton>
- #include <QString>
- #include <QVBoxLayout>
- #include <QHBoxLayout>
+#include <vector>
+#include <set>
+#include <QLabel>
+#include <QPushButton>
+#include <QString>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
- #ifdef Q_OS_WIN
-  #include <SDL.h>
- #else // use lib from OS on other systems
-  #include <SDL/SDL.h>
- #endif
- #include "common.h"
- #include "shared/hexfile.h"
+#ifdef Q_OS_WIN
+    #include <SDL.h>
+#else // use lib from OS on other systems
+    #include <SDL/SDL.h>
 #endif
+#include "common.h"
+#include "shared/hexfile.h"
