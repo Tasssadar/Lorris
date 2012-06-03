52bcb8183d16e79910225b5ed1f2273be7ac27a6
Tassadar <vbocek@gmail.com>
2012-06-03 17:42:56 +0200

    Add OS X build info

--- packer.sh
+++ packer.sh
@@ -5,7 +5,7 @@ rm index.html
 files=( head.html menu.html downloads.html desc.html build.html license.html bug.html screenshots.html bottom.html )
 for file in ${files[@]}
 do
-    echo "<!-- $file -->" >> index.html
+#    echo "<!-- $file -->" >> index.html
     cat $file >> index.html
     echo "" >> index.html
 done
