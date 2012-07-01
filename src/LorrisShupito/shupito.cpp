b5674461b00ec807dda1b5960b065f3c07d54ef8
Martin Vejnár <avakar@ratatanek.cz>
2012-07-01 22:16:39 +0200

    Fixed an effed up merge.

--- src/LorrisShupito/shupito.cpp
+++ src/LorrisShupito/shupito.cpp
@@ -1,25 +1,9 @@
-/****************************************************************************
+/**********************************************
+**    This file is part of Lorris
+**    http://tasssadar.github.com/Lorris/
 **
-**    This file is part of Lorris.
-**    Copyright (C) 2012 Vojtěch Boček
-**
-**    Contact: <vbocek@gmail.com>
-**             https://github.com/Tasssadar
-**
-**    Lorris is free software: you can redistribute it and/or modify
-**    it under the terms of the GNU General Public License as published by
-**    the Free Software Foundation, either version 3 of the License, or
-**    (at your option) any later version.
-**
-**    Lorris is distributed in the hope that it will be useful,
-**    but WITHOUT ANY WARRANTY; without even the implied warranty of
-**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**    GNU General Public License for more details.
-**
-**    You should have received a copy of the GNU General Public License
-**    along with Lorris.  If not, see <http://www.gnu.org/licenses/>.
-**
-****************************************************************************/
+**    See README and COPYING
+***********************************************/
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -57,8 +41,6 @@ void Shupito::init(ShupitoConnection *con, ShupitoDesc *desc)
 {
     m_con = con;
     m_desc = desc;
-
-    //chip_definition::parse_default_chipsets(m_chip_defs);
 
     ShupitoPacket getInfo = makeShupitoPacket(MSG_INFO, 1, 0x00);
     QByteArray data = waitForStream(getInfo, MSG_INFO);
