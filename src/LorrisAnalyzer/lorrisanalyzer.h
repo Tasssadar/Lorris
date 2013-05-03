6eb14cf4516ea7aa95027c6ced43ce22191dd864
Vojtech Bocek <vbocek@gmail.com>
2013-05-03 20:40:14 +0200

    Fix indentation in lorrisanalyzer.h

--- src/LorrisAnalyzer/lorrisanalyzer.h
+++ src/LorrisAnalyzer/lorrisanalyzer.h
@@ -37,92 +37,92 @@ enum hideable_areas
 };
 
 namespace Ui {
-  class LorrisAnalyzer;
+    class LorrisAnalyzer;
 }
 
 class LorrisAnalyzer : public PortConnWorkTab
 {
     Q_OBJECT
 
-    Q_SIGNALS:
-        void newData(analyzer_data *data, quint32 index);
-        void SendData(const QByteArray& data);
-        void rawData(const QByteArray& data);
-        void tinyWidgetBtn(bool tiny);
+Q_SIGNALS:
+    void newData(analyzer_data *data, quint32 index);
+    void SendData(const QByteArray& data);
+    void rawData(const QByteArray& data);
+    void tinyWidgetBtn(bool tiny);
 
-    public:
-        explicit LorrisAnalyzer();
-        virtual ~LorrisAnalyzer();
+public:
+    explicit LorrisAnalyzer();
+    virtual ~LorrisAnalyzer();
 
-        QString GetIdString();
+    QString GetIdString();
 
-        bool isAreaVisible(quint8 area);
-        void setAreaVisibility(quint8 area, bool visible);
-        analyzer_data *getLastData(quint32& idx);
-        QByteArray *getDataAt(quint32 idx);
-        analyzer_packet *getPacket() const { return m_packet; }
+    bool isAreaVisible(quint8 area);
+    void setAreaVisibility(quint8 area, bool visible);
+    analyzer_data *getLastData(quint32& idx);
+    QByteArray *getDataAt(quint32 idx);
+    analyzer_packet *getPacket() const { return m_packet; }
 
-        quint32 getCurrentIndex();
+    quint32 getCurrentIndex();
 
-        void setPortConnection(ConnectionPointer<PortConnection> const & con);
-        void openFile(const QString& filename);
+    void setPortConnection(ConnectionPointer<PortConnection> const & con);
+    void openFile(const QString& filename);
 
-        void saveData(DataFileParser *file);
-        void loadData(DataFileParser *file);
+    void saveData(DataFileParser *file);
+    void loadData(DataFileParser *file);
 
-        DataFilter *getFilter(quint32 id);
-        DataFilter *getFilterByOldInfo(const data_widget_infoV1& old_info) const;
+    DataFilter *getFilter(quint32 id);
+    DataFilter *getFilterByOldInfo(const data_widget_infoV1& old_info) const;
 
-    public slots:
-        void onTabShow(const QString& filename);
-        bool onTabClose();
-        void updateData();
-        void widgetMouseStatus(bool in, const data_widget_info &, qint32 parent);
-        void setDataChanged(bool changed = true) { m_data_changed = changed; }
+public slots:
+    void onTabShow(const QString& filename);
+    bool onTabClose();
+    void updateData();
+    void widgetMouseStatus(bool in, const data_widget_info &, qint32 parent);
+    void setDataChanged(bool changed = true) { m_data_changed = changed; }
 
-        void addChildTab(ChildTab *tab, const QString& name);
-        void removeChildTab(ChildTab *tab);
+    void addChildTab(ChildTab *tab, const QString& name);
+    void removeChildTab(ChildTab *tab);
 
-    private slots:
-        void doNewSource();
+private slots:
+    void doNewSource();
 
-        void saveButton();
-        void saveAsButton();
-        void exportBin();
-        void importBinAct();
-        void clearAllButton();
-        void clearDataButton();
-        void openFile();
-        void editStruture();
+    void saveButton();
+    void saveAsButton();
+    void exportBin();
+    void importBinAct();
+    void clearAllButton();
+    void clearDataButton();
+    void openFile();
+    void editStruture();
 
-        void collapseTopButton();
-        void collapseRightButton();
-        void collapseLeftButton();
+    void collapseTopButton();
+    void collapseRightButton();
+    void collapseLeftButton();
 
-        void connectedStatus(bool connected);
-        void indexChanged(int value);
+    void connectedStatus(bool connected);
+    void indexChanged(int value);
 
-        void updateForWidget();
+    void updateForWidget();
 
-    private:
-        void readData(const QByteArray& data);
-        bool load(QString& name, quint8 mask);
-        void importBinary(const QString& filename, bool reset = true);
-        void resetDevAndStorage(analyzer_packet *packet = NULL);
-        void setPacket(analyzer_packet *packet);
-        bool askToSave();
+private:
+    void readData(const QByteArray& data);
+    bool load(QString& name, quint8 mask);
+    void importBinary(const QString& filename, bool reset = true);
+    void resetDevAndStorage(analyzer_packet *packet = NULL);
+    void setPacket(analyzer_packet *packet);
+    bool askToSave();
 
-        Ui::LorrisAnalyzer *ui;
-        Storage m_storage;
-        analyzer_packet *m_packet;
-        PacketParser m_parser;
+    Ui::LorrisAnalyzer *ui;
+    Storage m_storage;
+    analyzer_packet *m_packet;
+    PacketParser m_parser;
 
-        bool m_data_changed;
-        qint32 m_curIndex;
+    bool m_data_changed;
+    qint32 m_curIndex;
 
-        ConnectButton * m_connectButton;
-        analyzer_data m_curData;
-        bool m_rightVisible;
+    ConnectButton * m_connectButton;
+    analyzer_data m_curData;
+    bool m_rightVisible;
 };
 
 #endif // LORRISANALYZER_H
